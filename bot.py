c2963fd9a255efded15cda662ff26526b5040649
smartwa <smartwacaleb@gmail.com>
2024-10-17 18:20:36 +0300

    feat: Update dependencies and add new features to bot.py
    fix: Correct import statements and update function signatures
    
    patch: Improve error handling and add delete functionality

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
-from telebot import TeleBot
 from fdown_api import Fdown
 from pydantic import BaseModel
 from dotenv import dotenv_values
-from telebot.types import Message
 from os import remove
 import typing as t
+import telebot
+import telebot.types as types
 
 
 class BotConfig(BaseModel):
@@ -18,39 +18,113 @@ class BotConfig(BaseModel):
 
 bot_config = BotConfig(**dotenv_values())
 
-bot = TeleBot(bot_config.token)
+bot = telebot.TeleBot(bot_config.token)
 
 help = (
     "Greetings %(user)s.\n"
     f"I can help you download Facebook videos under {bot_config.duration_limit} seconds. \n"
-    "Send me any video url and I will download and send it over to you."
+    "Send me any video url and I will download then send it over to you.\n\n"
+    "Made with ❤️ by @AlphaBei from Kenya 🇰🇪"
 )
 
 
+def inline_delete_button(message: types.Message) -> types.InlineKeyboardButton:
+    """Make delete button
+
+    Args:
+        message (types.Message): Message obj
+
+    Returns:
+        types.InlineKeyboardButton: Delete button.
+    """
+    button = types.InlineKeyboardButton(
+        text="🗑️", callback_data=f"del:{message.chat.id}:{message.id}"
+    )
+    return button
+
+
+def error_handler(help="An error occured and I couldn't complete that request!"):
+
+    def main(func):
+        def decorator(message: types.Message):
+            try:
+                return func(message)
+            except Exception as e:
+                print(e)
+                markup = types.InlineKeyboardMarkup()
+                markup.add(inline_delete_button(message))
+                return bot.reply_to(message, help, reply_markup=markup)
+
+        return decorator
+
+    return main
+
+
 @bot.message_handler(commands=["start", "help"])
-def echo_help(msg: Message):
-    return bot.reply_to(msg, help % dict(user=msg.from_user.username))
+def echo_help(msg: types.Message):
+    markup = types.InlineKeyboardMarkup()
+    markup.add(inline_delete_button(msg))
+    markup.add(
+        types.InlineKeyboardButton("Contact Developer", url="https://t.me/AlphaBei")
+    )
+    return bot.reply_to(
+        msg, help % dict(user=msg.from_user.username), reply_markup=markup
+    )
 
 
 @bot.message_handler(regexp=r"https://.+\.facebook\.com.+?mibextid=\w{16}")
-def download_and_send_video(msg: Message):
+@error_handler()
+def download_and_send_video(msg: types.Message):
     f = Fdown()
     video_links = f.get_links(msg.text)
     saved_to = f.download_video(
         video_links,
         progress_bar=False,
     )
-    thumbnail = f.session.get(video_links.cover_photo).contents
+    thumbnail = f.session.get(video_links.cover_photo).content
+    markup = types.InlineKeyboardMarkup()
+    markup.add(inline_delete_button(msg))
+
     bot.send_video(
         msg.chat.id,
         open(saved_to, "wb"),
         thumbnail=thumbnail,
         caption=video_links.title,
+        reply_markup=markup,
     )
     remove(saved_to)
+    return
+
+
+@bot.message_handler(func=lambda msg: True)
+def any_other_text(msg):
+    markup = types.InlineKeyboardMarkup()
+    markup.add(inline_delete_button(msg))
+    bot.reply_to(
+        msg,
+        text="Kindly send a me a valid link to a Facebook video.",
+        reply_markup=markup,
+    )
+    return
+
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith("del:"))
+def delete_button_callback_handler(call: types.CallbackQuery):
+    """Deletes a sent message"""
+    _, chat_id, msg_id = call.data.split(":")
+    try:
+        bot.delete_message(chat_id, msg_id)
+        bot.delete_message(call.message.chat.id, call.message.id)
+    except Exception as e:
+        try:
+            bot.delete_message(call.message.chat.id, call.message.id)
+        except:
+            pass
+        pass
 
 
 if __name__ == "__main__":
+    print("Infinity polling ...")
     bot.infinity_polling(
         timeout=bot_config.timeout,
         skip_pending=bot_config.skip_pending,
